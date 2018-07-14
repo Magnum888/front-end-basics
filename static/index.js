94dc5f850b78b6246c1f3ebf9280c63949190770
Magnum888 <K.O.M.888@i.ua>
2018-07-14 22:52:27 +0300

    clear index.html and created booksManadger

--- static/index.js
+++ static/index.js
@@ -1,1 +1,20 @@
-console.log("hi")
+fetch("./static/data.json").then(response => {
+    (response.ok) ? response.json().then(json => { 
+        let books = json; 
+        booksManadger(books);
+      })
+    : console.log('Network request for data.json failed with response ' + response.status + ': ' + response.statusText);
+});
+
+
+booksManadger = (books) =>{
+    let myBooks = books,
+        countBook = 0,
+        left = document.querySelector('.left'),
+        right = document.querySelector('.right'),
+        countBookLocalStorage = +localStorage.getItem("countBook");
+  
+    createNode = (element) => document.createElement(element);
+    append = (parent, el) => parent.appendChild(el);
+    
+}
