9198e0c3e85a6b69558005abf7e5cdcd77285e25
Magnum888 <K.O.M.888@i.ua>
2018-07-14 22:57:14 +0300

    created function showBooks

--- static/index.js
+++ static/index.js
@@ -16,5 +16,56 @@ booksManadger = (books) =>{
   
     createNode = (element) => document.createElement(element);
     append = (parent, el) => parent.appendChild(el);
+
+    if(countBookLocalStorage == 0) {
+        for(var key in myBooks) {
+          if(myBooks.hasOwnProperty(key)) {
+              myBooks[key].id = key;
+              countBook = countBook+1;
+              myBooks[key].side = "left";
+              localStorage.setItem(myBooks[key].id, JSON.stringify(myBooks[key]));
+           }
+        }
+        localStorage.setItem("countBook", countBook);
+        showBooks();
+      }else{
+        countBook = countBookLocalStorage;
+        showBooks();
+      }
+
+
+      function showBooks (){
+        left.innerHTML = "";
+        right.innerHTML = "";
+        
+        for(let i = 1; i <= countBook; i++){
+          let newBook = JSON.parse(localStorage.getItem(i)),
+              div = createNode('div'),
+              elementDOM = `
+                <div class="pic"><span><img src="${newBook.img}"></span></div>
+                <div class="title">
+                  <span><b>Название:</b> "${newBook.name}"</span>
+                  <span><b>Автор:</b> ${newBook.author}</span>
+                </div>`;
     
+          div.className = "item";  
+          div.innerHTML = elementDOM;
+          div.dataset.id = newBook.id;
+    
+          if(newBook.side == "left"){
+            div.insertAdjacentHTML("beforeEnd", `<div class="after"></div>`);
+            newBook.side = "left";
+            localStorage.setItem(newBook.id, JSON.stringify(newBook));
+            append(left, div);
+          }else if(newBook.side == "right"){
+            div.insertAdjacentHTML("beforeEnd", `<div class="before" onclick="moveToLeft(`+newBook.id+`)"></div>`);
+            newBook.side = "right";
+            localStorage.setItem(newBook.id, JSON.stringify(newBook));
+            append(right, div);
+          }
+        }
+        let buttonAfter = document.querySelectorAll('.after');
+        buttonAfter.forEach((itemAfter) => itemAfter.addEventListener('click', moveToRight));
+        
+      }
 }
